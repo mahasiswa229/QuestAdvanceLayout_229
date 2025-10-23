57ac4011164bc22a185347860b387255e9b113f7
mahasiswa229 <mafaqih1945@gmail.com>
2025-10-23 09:18:24 +0700

    composable

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -15,3 +15,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun AktivitasPertama(modifier: Modifier) {
+
+}
