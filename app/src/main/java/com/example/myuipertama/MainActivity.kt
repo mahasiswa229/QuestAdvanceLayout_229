781ba8fc2a057cff8db0f380fd3da2c8c70555ce
mahasiswa229 <mafaqih1945@gmail.com>
2025-10-24 08:30:36 +0700

    aktifitas pertama

--- app/src/main/java/com/example/myuipertama/MainActivity.kt
+++ app/src/main/java/com/example/myuipertama/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyUIPertamaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Mo",
+                    AktivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
